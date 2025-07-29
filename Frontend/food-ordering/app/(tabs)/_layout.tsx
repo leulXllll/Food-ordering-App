f5414b185fb9cfff3639bb2653dcb2fd199f5851
Leul <leulzewduchanie@gmail.com>
2025-07-29 17:30:15 +0300

    style(tab): add style for tab

--- Frontend/food-ordering/app/(tabs)/_layout.tsx
+++ Frontend/food-ordering/app/(tabs)/_layout.tsx
@@ -1,54 +1,99 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+
+import { Text, View, StyleSheet } from 'react-native';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
-//'#F4A940'
-//'#1e4d4aff'
+const ICON_SIZE = 28;
 
 export default function TabLayout() {
-
   return (
     <Tabs
       screenOptions={{
         tabBarStyle: {
           backgroundColor: 'white',
-          marginBottom: 10,   
-          borderTopLeftRadius: 15,
-          borderTopRightRadius: 15,
-          height: 60,
+          marginBottom: 40,
+          marginHorizontal: 10,
+          borderRadius: 15,
+          height: 80,
+          paddingTop: 17,
+          paddingBottom: 17,
           position: 'absolute',
-        }
-      }}>
+        },
+        tabBarActiveTintColor: '#fff',
+        tabBarInactiveTintColor: 'gray',
+        tabBarShowLabel: false,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome5 name="home" size={24} color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <View style={{
+              alignItems: 'center', display: 'flex', flexDirection: 'row',
+              borderRadius: 10,
+              backgroundColor: focused ? '#F4A940' : 'transparent',
+              paddingHorizontal: focused ? 8 : 5
+            }}>
+              <FontAwesome5 name="home" size={ICON_SIZE} color={color} />
+              {focused && <Text style={{ fontSize: 10, color }}>Home</Text>}
+            </View>
+          ),
         }}
       />
       <Tabs.Screen
         name="order"
         options={{
-          title: 'Order',
-          tabBarIcon: ({ color }) => <MaterialIcons name="receipt-long" size={24} color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <View style={{
+              alignItems: 'center', display: 'flex', flexDirection: 'row',
+              borderRadius: 10,
+              backgroundColor: focused ? '#F4A940' : 'transparent',
+              paddingHorizontal: focused ? 8 : 5
+            }}>
+              <MaterialIcons name="receipt-long" size={ICON_SIZE} color={color} />
+              {focused && <Text style={{ fontSize: 10, color }}>Order</Text>}
+            </View>
+          ),
+
         }}
       />
       <Tabs.Screen
         name="setting"
         options={{
-          title: 'Setting',
-          tabBarIcon: ({ color }) => <MaterialIcons name="settings" size={24} color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <View style={{
+              alignItems: 'center', display: 'flex', flexDirection: 'row',
+              borderRadius: 10,
+              backgroundColor: focused ? '#F4A940' : 'transparent',
+              paddingHorizontal: focused ? 8 : 5
+            }}>
+              <MaterialIcons name="settings" size={ICON_SIZE} color={color} />
+              {focused && <Text style={{ fontSize: 10, color }}>Setting</Text>}
+            </View>
+          ),
         }}
       />
       <Tabs.Screen
         name="person"
         options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <MaterialIcons name="person" size={24} color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <View style={{
+              alignItems: 'center', display: 'flex', flexDirection: 'row',
+              borderRadius: 10,
+              backgroundColor: focused ? '#F4A940' : 'transparent',
+              paddingHorizontal: focused ? 8 : 5
+            }}>
+              <MaterialIcons name="person" size={ICON_SIZE} color={color} />
+              {focused && <Text style={{ fontSize: 10, color }}>Profile</Text>}
+            </View>
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+const style = StyleSheet.create({
+
+})

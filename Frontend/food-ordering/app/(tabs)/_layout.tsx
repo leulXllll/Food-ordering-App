ce400d890968de1e4f3ad0366c4d35a2cd7cbbd3
Leul <leulzewduchanie@gmail.com>
2025-07-29 15:56:25 +0300

    refactor(frontend): change app name to food ordering and initialize

---
+++ Frontend/food-ordering/app/(tabs)/_layout.tsx
@@ -0,0 +1,40 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+//'#F4A940'
+//'#1e4d4aff'
+
+export default function TabLayout() {
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: 'white',
+          marginBottom: 10,   
+          borderTopLeftRadius: 15,
+          borderTopRightRadius: 15,
+          height: 60,
+          position: 'absolute',
+        }
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome5 name="home" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="order"
+        options={{
+          title: 'Order',
+          tabBarIcon: ({ color }) => <MaterialIcons name="receipt-long" size={24} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}

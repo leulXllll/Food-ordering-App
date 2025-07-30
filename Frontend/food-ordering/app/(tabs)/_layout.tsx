da179f17cd2c1d8dc932025e1eb398833aef8f3d
Leul <leulzewduchanie@gmail.com>
2025-07-30 00:58:36 -0700

    style(tab): prevent icon and icon name overlap

--- Frontend/food-ordering/app/(tabs)/_layout.tsx
+++ Frontend/food-ordering/app/(tabs)/_layout.tsx
@@ -4,20 +4,26 @@ import { Text, View, StyleSheet } from 'react-native';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
-const ICON_SIZE = 28;
+const ICON_SIZE = 24;
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
+        tabBarItemStyle: {
+         flex: 1,
+         maxWidth: 120,
+         justifyContent: 'center',
+         alignItems: 'center',
+        },
         tabBarStyle: {
           backgroundColor: 'white',
           marginBottom: 40,
-          marginHorizontal: 10,
           borderRadius: 15,
           height: 80,
-          paddingTop: 17,
-          paddingBottom: 17,
+          paddingTop: 15,
+          paddingBottom: 12,
+          paddingHorizontal: 5,
           position: 'absolute',
         },
         tabBarActiveTintColor: '#fff',
@@ -34,7 +40,6 @@ export default function TabLayout() {
               alignItems: 'center', display: 'flex', flexDirection: 'row',
               borderRadius: 10,
               backgroundColor: focused ? '#F4A940' : 'transparent',
-              paddingHorizontal: focused ? 8 : 5
             }}>
               <FontAwesome5 name="home" size={ICON_SIZE} color={color} />
               {focused && <Text style={{ fontSize: 10, color }}>Home</Text>}
@@ -50,7 +55,6 @@ export default function TabLayout() {
               alignItems: 'center', display: 'flex', flexDirection: 'row',
               borderRadius: 10,
               backgroundColor: focused ? '#F4A940' : 'transparent',
-              paddingHorizontal: focused ? 8 : 5
             }}>
               <MaterialIcons name="receipt-long" size={ICON_SIZE} color={color} />
               {focused && <Text style={{ fontSize: 10, color }}>Order</Text>}
@@ -67,7 +71,6 @@ export default function TabLayout() {
               alignItems: 'center', display: 'flex', flexDirection: 'row',
               borderRadius: 10,
               backgroundColor: focused ? '#F4A940' : 'transparent',
-              paddingHorizontal: focused ? 8 : 5
             }}>
               <MaterialIcons name="settings" size={ICON_SIZE} color={color} />
               {focused && <Text style={{ fontSize: 10, color }}>Setting</Text>}
@@ -83,7 +86,6 @@ export default function TabLayout() {
               alignItems: 'center', display: 'flex', flexDirection: 'row',
               borderRadius: 10,
               backgroundColor: focused ? '#F4A940' : 'transparent',
-              paddingHorizontal: focused ? 8 : 5
             }}>
               <MaterialIcons name="person" size={ICON_SIZE} color={color} />
               {focused && <Text style={{ fontSize: 10, color }}>Profile</Text>}
